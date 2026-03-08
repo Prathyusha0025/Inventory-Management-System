35157604ce0c6a69be99e8f0075c7aadf9c69746
Prathyusha0025 <sprathyusha134@gmail.com>
2026-03-08 17:07:03 -0400

    Add files via upload

---
+++ models.py
@@ -0,0 +1,7 @@
+from database import db
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    reorder_level = db.Column(db.Integer, nullable=False)
